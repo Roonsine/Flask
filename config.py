4757661808011bf416f6cc868d6b7d8456b7936f
Roonsine <synyster.rias@gmail.com>
2023-03-30 19:48:13 -0400

    Finished chapter 2

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 class Config(object): 
-    pass 
- 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
 class ProdConfig(Config): 
     pass 
- 
+
+
 class DevConfig(Config): 
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
