f7d723aa0b39afdb729b765ce298315337dac960
Roonsine <synyster.rias@gmail.com>
2023-03-30 21:26:03 -0400

    Added Templates and the entire base look of the blog

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
-class Config(object): 
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+class Config(object):
+    POSTS_PER_PAGE = 10
 
 
 class ProdConfig(Config): 
-    pass 
+    SECRET_KEY = "U\x16\xc6>\xb9*\r'R\xae\x1a\xd1]\xbc\x1cx\x00`\x12\x93\xbb\x1ff/"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
 
 
 class DevConfig(Config): 
     DEBUG = True
+    SECRET_KEY = 'Y\x8ek\x88\xb1\xc50\x9b\x11Z\xfbg\x02\x0c\xb0*\xb2r\x10\xa8\x9c\x0e\x84!'
     SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SQLALCHEMY_ECHO = True
 
